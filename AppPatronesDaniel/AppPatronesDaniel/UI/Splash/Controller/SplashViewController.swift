9484aa08aee7c974c74f85d1a0c730d7c5c832a2
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-04 16:48:04 +0200

    Splash View Model making:  Basic Structure

--- AppPatronesDaniel/AppPatronesDaniel/UI/Splash/Controller/SplashViewController.swift
+++ AppPatronesDaniel/AppPatronesDaniel/UI/Splash/Controller/SplashViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class SplashViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }
---
+++ AppPatronesDaniel/AppPatronesDaniel/UI/Splash/ViewModel/SplashViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewModel.swift
+//  AppPatronesDaniel
+//
+//  Created by Daniel Cazorro Frías on 4/10/23.
+//
+
+import Foundation
+
+// MARK: - Protocol
+
+// MARK: - Class
+final class SplashViewModel {
+    
+}
+
+// MARK: - Extension
