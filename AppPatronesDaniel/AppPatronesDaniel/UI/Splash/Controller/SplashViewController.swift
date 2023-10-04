eb75bdfe53db1a1bb34c704c662af3fbe16b4d44
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-04 17:21:11 +0200

    Make func navigateToHome and white homeView

--- AppPatronesDaniel/AppPatronesDaniel/UI/Splash/Controller/SplashViewController.swift
+++ AppPatronesDaniel/AppPatronesDaniel/UI/Splash/Controller/SplashViewController.swift
@@ -6,22 +6,39 @@
 //
 
 import UIKit
+
+
 // MARK: - Protocol
+protocol SplashViewProtocol {
+    func showLoading(_ show: Bool)
+    func navigateToHome()
+}
+
+
+
 
 // MARK: - Class
 class SplashViewController: UIViewController {
 
-    // MARK: - IBOutlets
+    // MARK: IBOutlets
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
     }
-    
-    
-
 }
 
 // MARK: - Extension
+extension SplashViewController: SplashViewProtocol {
+    func showLoading(_ show: Bool) {
+        
+    }
+    
+    func navigateToHome() {
+        let nextVC = HomeTableViewController()
+        navigationController?.setViewControllers([nextVC], animated: true)
+    }
+    
+    
+}
