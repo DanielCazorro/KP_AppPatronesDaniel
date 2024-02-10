ef15da52879fa1e4fbeeafbb566be7a1a397bb78
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-02-10 13:08:08 +0100

    Add comments to splash

--- AppPatronesDaniel/AppPatronesDaniel/UI/Splash/ViewModel/SplashViewModel.swift
+++ AppPatronesDaniel/AppPatronesDaniel/UI/Splash/ViewModel/SplashViewModel.swift
@@ -9,33 +9,32 @@ import Foundation
 
 // MARK: - Protocol
 protocol SplashViewModelProtocol {
-    func onViewsLoaded()
+    func onViewsLoaded() // Método para indicar que la vista ha cargado
 }
 
 
 // MARK: - Class
 final class SplashViewModel {
     
-    private weak var viewDelegate: SplashViewProtocol?
-    
+    private weak var viewDelegate: SplashViewProtocol? // Delegado de la vista
+
     init(viewDelegate: SplashViewProtocol?) {
         self.viewDelegate = viewDelegate
     }
     
     private func loadData() {
-        viewDelegate?.showLoading(true)
+        viewDelegate?.showLoading(true) // Mostrar indicador de carga
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) { [weak self] in
-            self?.viewDelegate?.showLoading(false)
-            self?.viewDelegate?.navigateToHome()
+            self?.viewDelegate?.showLoading(false) // Ocultar indicador de carga después de 2 segundos
+            self?.viewDelegate?.navigateToHome() // Navegar a la pantalla de inicio
         }
     }
 }
 
-
 // MARK: - Extension
 extension SplashViewModel: SplashViewModelProtocol {
     func onViewsLoaded() {
-        loadData()
+        loadData() // Llama a la función para cargar datos cuando la vista ha cargado
     }
     
 }
